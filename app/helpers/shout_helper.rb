f8ec486845f0d154c0231f9a2275e358f2ac74dc
Brendan Thomas <bt331609@gmail.com>
2018-11-23 16:44:39 -0500

    extract avatar helper to its own file

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -3,13 +3,6 @@ module ShoutHelper
     text.gsub(/@\w+/) { |mention| link_to mention, user_path(mention[1..-1]) }.html_safe
   end
 
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest user.email
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-
-    image_tag gravatar_url
-  end
-
   def like_button(shout)
     if current_user.liked? shout
       link_to "Unlike", unlike_shout_path(shout), method: :delete
